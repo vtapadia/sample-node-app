a98cd01de20611cd21b243ce43816fe1a6175cd5
Varesh Tapadia <varesh.tapadia@verifone.com>
2020-05-02 12:17:11 +0200

    Hello World Server

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var msg = 'Hello World';
-console.log(msg);
+// var msg = 'Hello World';
+// console.log(msg);
 
+var express = require('express');
+var server = {
+    port: 3000
+};
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(server.port, function () {
+  console.log('Example app listening on port %d!', server.port);
+});
