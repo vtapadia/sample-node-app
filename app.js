a6f57c653a0d732d3665f59afa7f603a4add3b42
Varesh Tapadia <varesh.tapadia@verifone.com>
2020-05-04 10:25:44 +0200

    Added API Calls using http inbuilt object

--- app.js
+++ app.js
@@ -14,9 +14,34 @@ app.get('/', function (req, res) {
   }
 );
 
+const http = require('http');
+
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    http.get(url, (res) => {
+      var data ='';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        console.log("Got data %s", data);
+        resolve(data);
+      });
+      res.on('error', (err) => {
+        console.error(err);
+        reject("failed to execute URL");
+      });
+    });
+  });
+}
+
 app.get('/api/name', function (req, res) {
-  //TODO Get first name and last name, combine and respond.
-  res.send('Varesh Tapadia');
+  var fName = get('http://localhost:3000/api/name/first');
+  var lName = get('http://localhost:3000/api/name/last');
+  Promise.all([fName, lName]).then(function(values) {
+    res.send(values[0] + " " + values[1] + "!!!");
+  })
+  // res.send(fName + " " + lName + "!!!");
 });
 
 app.get('/api/name/first', function (req, res) {
