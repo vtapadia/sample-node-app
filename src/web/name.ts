ede7e6a01c5cb0dcaaa5e45d36ce2002026fec72
Varesh Tapadia <varesh.tapadia@verifone.com>
2020-05-17 09:41:30 +0200

    Modified API to return JSON

--- src/web/name.ts
+++ src/web/name.ts
@@ -1,17 +1,18 @@
 import { Response, Request} from "express";
 import logger from "../config/logger";
+import {Message} from "../models/resources"
 
 export const firstName = (req: Request, res: Response) => {
   logger.debug("FirstName API called");
-  res.send("Varesh");
+  res.json(new Message("Varesh"));
 }
 
 export const lastName = (req: Request, res: Response) => {
-  res.send("Tapadia");
+  res.json(new Message("Tapadia"));
 }
 
 export const name = (req: Request, res: Response) => {
   //TODO call first and last and then submit the result
   logger.debug("Full Name API called");
-  res.send("Varesh Tapadia");
+  res.json(new Message("Varesh Tapadia"));
 }
